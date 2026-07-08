767fc77405bc296f3d6adc71cb5641b87e27b360
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-08 19:30:39 -0400

    removed discord from tty systems

--- users/tressa/modules/discord.nix
+++ users/tressa/modules/discord.nix
@@ -1,26 +1,28 @@
-{ lib, pkgs, inputs, user, ... }: {
+{ lib, pkgs, host, inputs, user, ... }: {
   imports = [ inputs.nixcord.homeModules.nixcord ];
 
-  programs.nixcord = {
-    enable = true;
-    user = user.name;
-    config.frameless = true;
-    discord = {
-      silenceNoModClientWarning = true;
-      krisp.enable = true;
+  config = lib.optionalAttrs (host.session != null) {
+    programs.nixcord = {
+      enable = true;
+      user = user.name;
+      config.frameless = true;
+      discord = {
+        silenceNoModClientWarning = true;
+        krisp.enable = true;
+      };
     };
-  };
 
-  home = {
-    activation.linkDiscordState = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
-      if [ -d $HOME/.config/discord ] && [ ! -L $HOME/.config/discord ]; then
-        $DRY_RUN_CMD cp -r $HOME/.config/discord $HOME/.var/config/
-        $DRY_RUN_CMD rm -r $HOME/.config/discord
-      fi
-      if [ -e $HOME/.config/discord ]; then
-        $DRY_RUN_CMD rm $HOME/.config/discord
-      fi
-      $DRY_RUN_CMD ln -sr $HOME/.var/config/discord $HOME/.config/discord
-    '';
+    home = {
+      activation.linkDiscordState = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+        if [ -d $HOME/.config/discord ] && [ ! -L $HOME/.config/discord ]; then
+          $DRY_RUN_CMD cp -r $HOME/.config/discord $HOME/.var/config/
+          $DRY_RUN_CMD rm -r $HOME/.config/discord
+        fi
+        if [ -e $HOME/.config/discord ]; then
+          $DRY_RUN_CMD rm $HOME/.config/discord
+        fi
+        $DRY_RUN_CMD ln -sr $HOME/.var/config/discord $HOME/.config/discord
+      '';
+    };
   };
 }
