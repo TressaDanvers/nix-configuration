2b12bb7e2e2ce57df88178c00051d2e000baf2ad
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-08 20:14:01 -0400

    added onefetch

--- users/tressa/modules/home.nix
+++ users/tressa/modules/home.nix
@@ -2,9 +2,12 @@
   programs = {
     bash = {
       enable = true;
-      shellAliases.ff = "clear; fastfetch";
+      shellAliases = {
+        ff = "clear; fastfetch";
+        of = "clear; onefetch";
+      };
     };
   };
 
-  home.packages = with pkgs; [ fastfetch tree ];
+  home.packages = with pkgs; [ fastfetch onefetch tree ];
 }
