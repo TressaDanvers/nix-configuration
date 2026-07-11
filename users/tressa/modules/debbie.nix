9031b3e901e975c83d62348805776d17921e32c3
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-10 20:01:15 -0400

    added voices of the void shortcut

--- users/tressa/modules/debbie.nix
+++ users/tressa/modules/debbie.nix
@@ -1,5 +1,14 @@
-{ lib, host, ... }: {
+{ lib, host, config, ... }: {
   config = lib.optionalAttrs (host.name == "debbie") {
-    
+    xdg.desktopEntries = {
+      votv = {
+        name = "Voices of the Void";
+        exec = "${config.home.homeDirectory}/.local/opt/votv/VotV.exe";
+        icon = "${config.home.homeDirectory}/.local/opt/votv/VotV.ico";
+        type = "Application";
+        terminal = false;
+        categories = [ "Game" ];
+      };
+    };
   };
 }
