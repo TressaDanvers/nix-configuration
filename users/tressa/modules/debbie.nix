4fa86066d3eb9c8dec44d3c0130af528a5e1b6cb
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-11 12:11:00 -0400

    gamescoped votv

--- users/tressa/modules/debbie.nix
+++ users/tressa/modules/debbie.nix
@@ -3,7 +3,7 @@
     xdg.desktopEntries = {
       votv = {
         name = "Voices of the Void";
-        exec = "${config.home.homeDirectory}/.local/opt/votv/VotV.exe";
+        exec = "gamescope -W 1920 -H 1080 ${config.home.homeDirectory}/.local/opt/votv/VotV.exe";
         icon = "${config.home.homeDirectory}/.local/opt/votv/VotV.ico";
         type = "Application";
         terminal = false;
