348fa9d113e0cf32f272f78d094ea8d47e791f9b
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-11 12:52:32 -0400

    moved votv icon to nix configuration directly

--- users/tressa/modules/debbie.nix
+++ users/tressa/modules/debbie.nix
@@ -4,7 +4,7 @@
       votv = {
         name = "Voices of the Void";
         exec = "gamescope -W 1920 -H 1080 ${config.home.homeDirectory}/.local/opt/votv/VotV.exe";
-        icon = "${config.home.homeDirectory}/.local/opt/votv/VotV.ico";
+        icon = ../resources/icons/VotV.ico;
         type = "Application";
         terminal = false;
         categories = [ "Game" ];
