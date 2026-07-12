baf6b33d1872b1c05b38bbfb7835f6da5a972aa1
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-12 00:14:52 -0400

    added youtube shorts redirect

--- users/tressa/modules/qutebrowser.nix
+++ users/tressa/modules/qutebrowser.nix
@@ -24,6 +24,27 @@
           url = "https://raw.githubusercontent.com/TressaDanvers/youtube-adb/refs/heads/main/index.user.js";
           sha256 = "sha256-HJacrbNhHOSIvkU4BJE30B26PSjiHO5mplfLjAd6RjM=";
         })
+        (writeText "no-shorts.js" ''
+           // ==UserScript==
+           // @name Youtube Shorts Redirect
+           // @match *://www.youtube.com/*
+           // @run-at document-start
+           // ==/UserScript==
+           //*/
+
+           function redirect() {
+             const url = new URL(window.location.href);
+             const match = url.pathname.match(/^\/shorts\/([^/]+)/);
+             if (match) {
+               url.pathname = '/watch';
+               url.searchParams.set('v', match[1]);
+               window.location.replace(url.href);
+             }
+           }
+
+           redirect();
+           document.addEventListener('yt-navigate-finish', redirect);
+        '')
       ];
     };
   };
