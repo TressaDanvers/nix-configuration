1c4700901b4c0db6631874fbed5147da19b4f6d8
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-12 13:13:19 -0400

    included librewolf on bspwm again for a gecko renderer to develop with

--- users/tressa/modules/librewolf.nix
+++ users/tressa/modules/librewolf.nix
@@ -1,5 +1,5 @@
 { config, host, lib, pkgs, ... }: {
-  config = lib.optionalAttrs (host.session != null && host.session != "bspwm") {
+  config = lib.optionalAttrs (host.session != null) {
     home.packages = with pkgs; [ librewolf ];
   };
 }
