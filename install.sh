cc9ff3e91c830a2699b069add94e8c37b8cc19b2
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-21 01:08:23 -0400

    allowed nix-command and flakes in the home-manager switch

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 git clone https://github.com/TressaDanvers/nix-configuration ~/.config/home-manager
-nix run --extra-experimental-features 'nix-command flakes' nixpkgs#home-manager switch
+nix run --extra-experimental-features 'nix-command flakes' nixpkgs#home-manager switch --extra-experimental-features 'nix-command flakes'
