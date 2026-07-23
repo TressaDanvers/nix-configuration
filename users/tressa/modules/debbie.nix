b6cd3adb184640670d16ea79fa3b737caa890a1b
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-23 16:39:41 -0400

    setup fallout 4

--- users/tressa/modules/debbie.nix
+++ users/tressa/modules/debbie.nix
@@ -1,9 +1,18 @@
 { lib, pkgs, host, config, ... }: {
   config = lib.optionalAttrs (host.name == "debbie") {
-    home.packages = with pkgs; [ (writeShellApplication { name = "factorio"; text = ''
-      CONFIG="$HOME/.config/home-manager/users/tressa/config/factorio/config.ini"
-      exec ${pkgs.umu-launcher}/bin/umu-run /opt/factorio/bin/x64/factorio --config "$CONFIG" "$@"
-    '';}) ];
+    home.packages = with pkgs; [
+      (writeShellApplication { name = "factorio"; text = ''
+        CONFIG="$HOME/.config/home-manager/users/tressa/config/factorio/config.ini"
+        exec ${pkgs.umu-launcher}/bin/umu-run /opt/factorio/bin/x64/factorio --config "$CONFIG" "$@"
+      '';})
+      (writeShellApplication { name = "fallout-4"; text = ''
+        if [ "$1" = "--launch" ]; then
+          exec ${pkgs.protontricks}/bin/protontricks-launch --appid 377160 /s/Modding/MO2/ModOrganizer.exe moshortcut://:F4SE
+        elif [ "$1" = "--mo2" ]; then
+          exec ${pkgs.protontricks}/bin/protontricks-launch --appid 377160 /s/Modding/MO2/ModOrganizer.exe
+        fi
+      '';})
+    ];
 
     xdg.desktopEntries = {
       votv = {
@@ -30,6 +39,20 @@
         icon = ../resources/icons/Factorio.ico;
         type = "Application";
         terminal = false;
+        categories = [ "Game" ];
+      };
+
+      fallout-4 = {
+        name = "Fallout 4";
+        exec = "fallout-4 --launch";
+        icon = ../resources/icons/Fallout-4.ico;
+        type = "Application";
+        terminal = false;
+        actions.mod-organizer = {
+          name = "ModOrganizer2";
+          exec = "fallout-4 --mo2";
+          icon = ../resources/icons/ModOrganizer2.ico;
+        };
         categories = [ "Game" ];
       };
     };
